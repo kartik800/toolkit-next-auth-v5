dc95e50dbb4c1993bf8ff89fc80c8f99e480451c
Kartik Aggarwal <er.kartik18102016@gmail.com>
2024-02-26 17:43:43 +0530

    added next-auth config

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server"
 
-import { RegisterSchema, RegisterSchemaType } from "@/schemas";
+import bcrypt from "bcryptjs"
+import   { RegisterSchema, RegisterSchemaType } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: RegisterSchemaType) => {
     const validatedField = RegisterSchema.safeParse(values);
@@ -10,5 +13,25 @@ export const register = async (values: RegisterSchemaType) => {
 
     }
 
-    return {success: "Emain Sent!"}
+    const {email, password, name} = validatedField.data;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+
+    if(existingUser){
+        return {error: "Email Already Exist"}
+    }
+
+    await db.user.create({
+        data: {
+            email,
+            name,
+            password: hashedPassword
+        }
+    });
+
+    // Todo: Sent verification token email
+
+    return {success: "User Created"}
 }
