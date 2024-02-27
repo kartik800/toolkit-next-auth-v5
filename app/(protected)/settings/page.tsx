506393d721c30daa54bf2a1c46729f2b4eb6b27d
Kartik Aggarwal <er.kartik18102016@gmail.com>
2024-02-27 15:05:48 +0530

    added next-auth callbacks

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,7 +1,21 @@
 
-const SettingPage = () => {
+import { signOut , auth} from "@/auth"
+const SettingPage = async () => {
+
+  const session = await auth();
+  
   return (
-    <div>page</div>
+    <div>
+      <div>{JSON.stringify(session)}</div>
+    <form action={async ()=> {
+        "use server"
+        await signOut();
+    }}>
+      <button type="submit">
+        Sign Out
+      </button>
+    </form>
+    </div>
   )
 }
 
